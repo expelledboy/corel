bf46a43c99f66c45750f1b5ae13833be37c99336
expelledboy <expelledboy+github@gmail.com>
2014-11-06 00:16:56 +0200

    flipping dbg flag

--- include/debug.hrl
+++ include/debug.hrl
@@ -1,5 +1,5 @@
 -ifdef(DEBUG).
--define(dbg(S, As), ok).
--else.
 -define(dbg(S, As), (begin io:fwrite(user, <<"\t~s:~w:~w: ~s\n">>, [?FILE, ?LINE, self(), io_lib:format((S), (As))]), ok end)).
+-else.
+-define(dbg(S, As), ok).
 -endif.
